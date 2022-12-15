a883c2754184218f67f31a1a36be3a51305a3fd6
Naoya Yasuda <43776161+yanskun@users.noreply.github.com>
2022-12-16 00:57:14 +0900

    feat: add args

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,15 @@
 mod cmd;
+use std::env;
 
 fn main() {
-    let tag = cmd::increment_version(cmd::SemanticVersion::Major);
+    let arg = env::args().nth(1).unwrap();
+    let semantic = match arg.as_str() {
+        "--major" => cmd::SemanticVersion::Major,
+        "--minor" => cmd::SemanticVersion::Minor,
+        "--patch" => cmd::SemanticVersion::Patch,
+        _ => panic!("Invalid argument"),
+    };
+
+    let tag = cmd::increment_version(semantic);
     println!("{}", tag);
 }
