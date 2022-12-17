9b7254d48466b11a8289d1d7a28ad5783afcb7ee
Naoya Yasuda <43776161+yanskun@users.noreply.github.com>
2022-12-17 14:21:45 +0900

    feat: short option

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,9 @@ use std::env;
 fn main() {
     let arg = env::args().nth(1).unwrap();
     let semantic = match arg.as_str() {
-        "--major" => cmd::SemanticVersion::Major,
-        "--minor" => cmd::SemanticVersion::Minor,
-        "--patch" => cmd::SemanticVersion::Patch,
+        "--major" | "-M" => cmd::SemanticVersion::Major,
+        "--minor" | "-m" => cmd::SemanticVersion::Minor,
+        "--patch" | "-p" => cmd::SemanticVersion::Patch,
         _ => panic!("Invalid argument"),
     };
 
