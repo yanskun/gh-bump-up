d468d0eb28c4cacb8b1e0624a7712fe447e97188
Naoya Yasuda <43776161+yanskun@users.noreply.github.com>
2022-12-18 12:57:58 +0900

    fix: sort

--- src/cmd.rs
+++ src/cmd.rs
@@ -7,35 +7,6 @@ pub enum SemanticVersion {
     Patch,
 }
 
-fn get_latest_tag() -> String {
-    let gh_release_first = Command::new("gh")
-        .arg("release")
-        .arg("list")
-        .stdout(Stdio::piped())
-        .spawn()
-        .unwrap();
-    let grep_latest = Command::new("grep")
-        .arg("Latest")
-        .stdin(gh_release_first.stdout.unwrap())
-        .stdout(Stdio::piped())
-        .spawn()
-        .unwrap();
-    let awk_tag = Command::new("awk")
-        .arg("{print $1}")
-        .stdin(grep_latest.stdout.unwrap())
-        .stdout(Stdio::piped())
-        .spawn()
-        .unwrap();
-
-    let output = awk_tag.wait_with_output().unwrap();
-    let result = str::from_utf8(&output.stdout)
-        .unwrap()
-        .lines()
-        .next()
-        .unwrap();
-    result.to_string()
-}
-
 pub fn bump_version(semantic: SemanticVersion) -> String {
     let tag = get_latest_tag();
     let version: Vec<&str> = tag.split(".").collect();
@@ -70,4 +41,33 @@ pub fn bump_version(semantic: SemanticVersion) -> String {
         true => format!("v{}", new_version),
         false => new_version,
     }
+}
+
+fn get_latest_tag() -> String {
+    let gh_release_first = Command::new("gh")
+        .arg("release")
+        .arg("list")
+        .stdout(Stdio::piped())
+        .spawn()
+        .unwrap();
+    let grep_latest = Command::new("grep")
+        .arg("Latest")
+        .stdin(gh_release_first.stdout.unwrap())
+        .stdout(Stdio::piped())
+        .spawn()
+        .unwrap();
+    let awk_tag = Command::new("awk")
+        .arg("{print $1}")
+        .stdin(grep_latest.stdout.unwrap())
+        .stdout(Stdio::piped())
+        .spawn()
+        .unwrap();
+
+    let output = awk_tag.wait_with_output().unwrap();
+    let result = str::from_utf8(&output.stdout)
+        .unwrap()
+        .lines()
+        .next()
+        .unwrap();
+    result.to_string()
 }
